f13b5b2a5fa0c634fc8a83c00d50741ed920f875
Alfonso Ballesteros <ponchob1258@gmail.com>
2019-11-20 01:42:25 -0500

    drawer navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() titulo: string;
+
   constructor() { }
 
   ngOnInit() {}
